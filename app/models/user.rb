27d492fc18b9dcfe0a9b3ee3dfaed42c059cf9d9
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-09-26 11:32:23 +0700

    Add model, validation and migration for User model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+  validates :username, uniqueness: true, length: { in: 4..12 }, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password,  length: { in: 6..16 }, presence: true
+end
